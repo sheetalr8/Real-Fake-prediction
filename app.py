a6bd144e516e6374266ff8b0a8e100549c8686b0
sheetalr8 <sheetal20028@gmail.com>
2026-02-11 15:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1x-Vn5OBQqG92S1SbgDjDmGRhwZtgjhqf
 """
 
-import sys
-!{sys.executable} -m pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
